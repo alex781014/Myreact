6334d73ad8e8e8e1feeef1929d8ed4d9e3b5ed75
alex hu <alex781014@gmail.com>
2022-06-21 11:36:43 +0800

    myreact1

--- src/page/Mypage.js
+++ src/page/Mypage.js
@@ -1,25 +1,33 @@
 import './Mypage.css'
 
+import { useState } from 'react'
+
 function Mypage() {
+    const [demoBG, setDemoBG] = useState('red')
+    function handleClick(e) {
+        console.log(e.target.style.backgroundColor);
+        setDemoBG(e.target.style.backgroundColor)
+    }
+
     return (
         <div className="container">
             <div className="row">
                 <div className="col-6">
-                    <div className="color-demo-area"></div>
+                    <div className="color-demo-area" style={{ backgroundColor: demoBG }}></div>
                 </div>
                 <div className="col-6">
                     <div className="color-selector-area d-flex flex-wrap">
                         <div className="col-3">
-                            <div className="color-selector autobg"></div>
+                            <div className="color-selector autobg" style={{ backgroundColor: 'salmon' }} onClick={handleClick}></div>
                         </div>
                         <div className="col-3">
-                            <div className="color-selector autobg"></div>
+                            <div className="color-selector autobg" style={{ backgroundColor: 'lightblue' }} onClick={handleClick}></div>
                         </div>
                         <div className="col-3">
-                            <div className="color-selector autobg"></div>
+                            <div className="color-selector autobg" onClick={handleClick}></div>
                         </div>
                         <div className="col-3">
-                            <div className="color-selector autobg"></div>
+                            <div className="color-selector autobg" onClick={handleClick}></div>
                         </div>
                     </div>
                 </div>
